dccab424674fa1ad9c69ac20fd4b8740f2bdb940
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-03-30 14:29:44 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 ||
+        typeof(num1) !== 'number' || typeof(num2) !== 'number')
+        {
+            return 'ERROR';
+        }
 
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+    let totalSum = 0;
+
+    for (let i = start; i <= end; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
