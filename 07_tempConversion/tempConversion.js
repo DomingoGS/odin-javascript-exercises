19d001cc00b1150bc25dd0284bb136393dc6825f
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-03-31 11:20:12 +0200

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitDegrees) {
+  const celsiusDegrees = (farenheitDegrees - 32) * 5 / 9;
+  return Math.round(celsiusDegrees * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  const farenheitDegrees = (celsiusDegrees * 9 / 5) + 32;
+  return Math.round(farenheitDegrees * 10) / 10;
 };
 
 // Do not edit below this line
