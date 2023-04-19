2c967682891962cf210e6788d5da7afe073194a2
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-04-19 17:43:13 +0200

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) return 0;
+  return array.reduce((sum, value) => sum + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) return 0;
+  return array.reduce((product, value) => product * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for(let n = 1; n <= number; n++) {
+    result *= n;
+  }
+  return result;
 };
 
 // Do not edit below this line
