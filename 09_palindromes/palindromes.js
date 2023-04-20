1855ed1212faab565094f54f68e6c05478de6280
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-04-20 10:57:38 +0200

    Complete exercise 09_Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[^\p{L}]/gu, '');
+    cleanStr = cleanStr.toLowerCase();
 
+    const reversedStr = cleanStr.split('').reverse().join('');
+
+    return cleanStr.localeCompare(reversedStr) === 0;
 };
 
 // Do not edit below this line
