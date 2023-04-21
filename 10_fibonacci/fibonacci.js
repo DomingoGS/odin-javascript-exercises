be812063738048ba190832899bcbb8f15b999c57
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-04-21 12:39:53 +0200

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(specificMember) {
+    if(specificMember < 1) return "OOPS";
+    let previousValue = 1;
+    let currentValue = 1;
+    let result;
+    for (member = 3; member <= specificMember; member++) {
+        result = previousValue + currentValue;
+        previousValue = currentValue;
+        currentValue = result;
+    }
+    return currentValue;
 };
 
 // Do not edit below this line
