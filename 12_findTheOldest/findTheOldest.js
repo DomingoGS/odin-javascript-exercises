839d50c222def1448ca5e700988e469d243691f7
Domingo Garcia <79866751+DomingoGS@users.noreply.github.com>
2023-04-22 14:09:06 +0200

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(
+        (oldest, person) => {
+            if(!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
+            if(!oldest.yearOfDeath) oldest.yearOfDeath = new Date().getFullYear();
+            return (person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth) ?
+                    person :
+                    oldest;
+        });
 };
 
 // Do not edit below this line
