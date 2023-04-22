d3a4ce5326498ae6cbca3fbebdcac49db1f6abb9
Domingo Garcia <79866751+DomingoGS@users.noreply.github.com>
2023-04-22 14:20:09 +0200

    Refactor exercise 12_findTheOldest completed code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,17 @@
-const findTheOldest = function(people) {
-    return people.reduce(
-        (oldest, person) => {
-            if(!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
-            if(!oldest.yearOfDeath) oldest.yearOfDeath = new Date().getFullYear();
-            return (person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth) ?
-                    person :
-                    oldest;
-        });
+const findTheOldest = function(array) {
+    return array.reduce((oldest, nextPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const nextPersonAge = getAge(
+            nextPerson.yearOfBirth, 
+            nextPerson.yearOfDeath);
+        return nextPersonAge > oldestAge ? nextPerson : oldest;
+    });
 };
+
+function getAge(birth, death) {
+    if(!death) death = new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
